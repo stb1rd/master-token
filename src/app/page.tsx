a6ba463a7249418ac4cd65abff5a0794cba01b86
Anton Vorobev <afvorobev@gmail.com>
2023-10-07 17:20:01 +0300

    feat: logo updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="hero min-h-screen -my-8">
         <div className="hero-content text-center w-full max-w-xl">
           <div className="flex flex-col w-full items-center gap-4">
-            <Image src="/NFT_Logo.svg" alt="NFT Logo" width={100} height={100} priority />
+            <Image src="/logo.jpeg" alt="NFT Logo" width={200} height={200} priority />
             <h1 className="text-5xl font-bold">MasterNFT</h1>
             <p className="text-2xl">the live history of any art/nft object</p>
             <Search />

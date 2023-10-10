b8f2ca5fc6f43c737df737b09c74f34c881ba327
Anton Vorobev <afvorobev@gmail.com>
2023-10-10 18:34:34 +0300

    fix: update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="hero min-h-screen -my-8">
         <div className="hero-content text-center w-full max-w-xl">
           <div className="flex flex-col w-full items-center gap-4">
-            <Image src="/logo.jpeg" alt="NFT Logo" width={200} height={200} priority />
+            <Image src="/icon-192.png" alt="NFT Logo" width={128} height={128} priority />
             <h1 className="text-5xl font-bold">MasterNFT</h1>
             <p className="text-2xl">the live history of any art/nft object</p>
             <Search />

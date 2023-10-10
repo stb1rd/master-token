1f2f19e6be4c55d30215d582d3bc08c49ed7f30d
Anton Vorobev <afvorobev@gmail.com>
2023-10-10 06:15:03 +0300

    fix: add simple auth

--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -8,6 +8,7 @@ import { useCallback, useEffect, useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
+import AuthForm from '@/components/AuthForm';
 
 export default function Index() {
   const supabase = createClientComponentClient();
@@ -34,77 +35,79 @@ export default function Index() {
   };
 
   return (
-    <div className="prose mx-auto">
-      <h1>CMS</h1>
-      <div className="text-sm breadcrumbs">
-        <ul className="pl-0">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>Content Management</li>
-        </ul>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          <tbody>
-            {products?.map((product) => (
-              <tr key={product.id} className="hover">
-                <td>{product.name}</td>
-                <td className="flex gap-1 justify-end">
-                  <button className="btn btn-sm">
-                    <Link href={`/catalog/${product.id}`}>
-                      <LaunchIcon fontSize="small" />
-                    </Link>
-                  </button>
-                  <button
-                    className="btn btn-sm"
-                    onClick={() => {
-                      setSelectedProduct(product);
-                      document.getElementById('product_form').showModal();
-                    }}
-                  >
-                    <EditIcon fontSize="small" />
-                  </button>
-                  <button className="btn btn-sm" onClick={() => handleDelete(product)}>
-                    <DeleteIcon fontSize="small" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="flex justify-center">
-        <button
-          className="btn btn-wide"
-          onClick={() => {
-            setSelectedProduct(null);
-            document.getElementById('product_form').showModal();
-          }}
-        >
-          Add new
-        </button>
-      </div>
-      <dialog id="product_form" className="modal">
-        <div className="modal-box  max-w-3xl">
-          <div className="flex items-center justify-between">
-            <h2 className="m-0">Product form</h2>
-            <form method="dialog">
-              <button className="btn">
-                <CloseIcon />
-              </button>
-            </form>
-          </div>
-          <ProductForm
-            product={selectedProduct}
-            onSuccess={() => {
-              getProducts();
-              document.getElementById('product_form').close();
-            }}
-          />
+    <AuthForm>
+      <div className="prose mx-auto">
+        <h1>CMS</h1>
+        <div className="text-sm breadcrumbs">
+          <ul className="pl-0">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>Content Management</li>
+          </ul>
         </div>
-      </dialog>
-    </div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <tbody>
+              {products?.map((product) => (
+                <tr key={product.id} className="hover">
+                  <td>{product.name}</td>
+                  <td className="flex gap-1 justify-end">
+                    <button className="btn btn-sm">
+                      <Link href={`/catalog/${product.id}`}>
+                        <LaunchIcon fontSize="small" />
+                      </Link>
+                    </button>
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => {
+                        setSelectedProduct(product);
+                        document.getElementById('product_form').showModal();
+                      }}
+                    >
+                      <EditIcon fontSize="small" />
+                    </button>
+                    <button className="btn btn-sm" onClick={() => handleDelete(product)}>
+                      <DeleteIcon fontSize="small" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            className="btn btn-wide"
+            onClick={() => {
+              setSelectedProduct(null);
+              document.getElementById('product_form').showModal();
+            }}
+          >
+            Add new
+          </button>
+        </div>
+        <dialog id="product_form" className="modal">
+          <div className="modal-box  max-w-3xl">
+            <div className="flex items-center justify-between">
+              <h2 className="m-0">Product form</h2>
+              <form method="dialog">
+                <button className="btn">
+                  <CloseIcon />
+                </button>
+              </form>
+            </div>
+            <ProductForm
+              product={selectedProduct}
+              onSuccess={() => {
+                getProducts();
+                document.getElementById('product_form').close();
+              }}
+            />
+          </div>
+        </dialog>
+      </div>
+    </AuthForm>
   );
 }
--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,75 +1,73 @@
 'use client';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-export default function AuthForm() {
-  const supabase = createClientComponentClient();
+const AuthForm = ({ children }) => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const router = useRouter();
+  const [isAuth, setIsAuth] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      setError('');
-      const { data, error } = await supabase.auth.signUp({
-        email: login,
-        password: password,
-      });
-      if (error) {
-        setError(error.message);
-      }
-      if (data) {
-        router.push('/cms');
-      }
-    } catch (e) {
-      setError(e.message);
+  const handleAuth = async () => {
+    setError('');
+    await new Promise<void>((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 2000)
+    );
+    if (login === process.env.NEXT_PUBLIC_AMIN_LOGIN && password === process.env.NEXT_PUBLIC_AMIN_PASSWORD) {
+      setIsAuth(true);
+    } else {
+      setError('invalid login or password');
     }
   };
 
-  return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
-      showLinks={false}
-      providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
-    />
-    // <form className="flex flex-col gap-4">
-    //   <div className="form-control w-full">
-    //     <label className="label">
-    //       <span className="label-text">Email</span>
-    //     </label>
-    //     <input
-    //       type="email"
-    //       autoComplete="email"
-    //       className="input input-bordered w-full"
-    //       value={login}
-    //       onChange={(e) => setLogin(e.target.value)}
-    //     />
-    //   </div>
-    //   <div className="form-control w-full">
-    //     <label className="label">
-    //       <span className="label-text">Password</span>
-    //     </label>
-    //     <input
-    //       type="password"
-    //       autoComplete="password"
-    //       className="input input-bordered w-full"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //   </div>
-    //   {!!error && <p>{error}</p>}
-    //   <button type="button" className="btn btn-primary" onClick={handleLogin}>
-    //     login
-    //   </button>
-    // </form>
-  );
-}
+  if (!isAuth) {
+    return (
+      // <Auth
+      //   supabaseClient={supabase}
+      //   view="magic_link"
+      //   appearance={{ theme: ThemeSupa }}
+      //   theme="dark"
+      //   showLinks={false}
+      //   providers={[]}
+      //   redirectTo="http://localhost:3000/auth/callback"
+      // />
+      <form className="flex flex-col gap-4 max-w-md mx-auto">
+        <h1>CMS login</h1>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            autoComplete="email"
+            className="input input-bordered w-full"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            autoComplete="password"
+            className="input input-bordered w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {!!error && <p>{error}</p>}
+        <button type="button" className="btn btn-primary" onClick={() => handleAuth()}>
+          login
+        </button>
+      </form>
+    );
+  }
+
+  return children;
+};
+
+export default AuthForm;
